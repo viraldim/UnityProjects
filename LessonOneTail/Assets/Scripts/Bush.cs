bd3a980009a0f882265797e7adadcb5852e11d6e
viraldim <viraldim@gmail.com>
2014-11-01 22:47:52 +0200

    First Homework From the Course
    Keyboard movable object chased by 1 object which is chased by another
    object which is chased by another object. (something like a tail).
    transform.Translate(..),
    transform.LookAt(..),
    Input.Getkey(...).

---
+++ LessonOneTail/Assets/Scripts/Bush.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class Bush : MonoBehaviour
+{
+    public float speed = 10;
+    public Transform cam;
+
+    void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	    if (Input.GetKey(KeyCode.W))
+	    {
+	        transform.Translate(0f, speed * Time.deltaTime, 0f);
+	    }
+	    if (Input.GetKey(KeyCode.A))
+	    {
+            transform.Translate(-speed * Time.deltaTime, 0f, 0f);
+	    }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(speed * Time.deltaTime, 0f, 0f);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0f, -speed * Time.deltaTime, 0f);
+        }
+	}
+
+    void LateUpdate()
+    {
+        cam.position = new Vector3(
+            transform.position.x+10f,
+            transform.position.y+10f,
+            transform.position.z-55f);
+    }
+}
---
+++ LessonOneTail/Assets/Scripts/Fireball.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.Serialization.Formatters;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+
+    public float speed;
+    public Transform target;
+    private Transform myTransform;
+
+    void Awake()
+    {
+        myTransform = transform;
+    }
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(target.position);
+
+        float distance = (target.position - myTransform.position).magnitude;
+        if (distance >=10)
+        {
+            myTransform.position += myTransform.forward * speed * Time.deltaTime;
+        }
+       
+        
+    }
+}
